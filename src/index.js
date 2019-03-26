d70d5af80289d12709349d2a2bc6ad6e0355e98d
pranil chhetri <pranilchhetri2014@gmail.com>
2019-03-26 12:23:07 +0545

    creating components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyDGfFJ1CybRVDuzt40j6abXaAy81wufubQ';
 //create a new component.producing some html
 const App= function(){
     return <div>Hi!</div>;
@@ -6,3 +10,4 @@ const App= function(){
 
 //take this component generated html and put it on the page in the DOM
 
+ReactDOM.render(<App />,document.querySelector('.container'));
