b24d444f872c88a0a15e912932267c0fecdfe787
pranil chhetri <pranilchhetri2014@gmail.com>
2019-03-27 14:50:07 +0545

    state change

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,14 +2,23 @@ import React, {Component } from 'react';
 
 
 class SearchBar extends Component{
-    render(){
-        return <input onChange={this.onInputChange} />;
+    constructor(props){
+        super(props);
+
+        this.state ={ term: '' };
     }
 
-onInputChange(event){
-    console.log(event.target.value);
-    }
-}
+    render(){
+        return (
+        <div>
+            <input 
+            value = {this.state.term}
+            onChange={event=> this.setState({term: event.target.value})} />
+            
+        </div>
+         );
+      }
+};
 //functional component
 // const SearchBar = () => {
 //     return <input />;
