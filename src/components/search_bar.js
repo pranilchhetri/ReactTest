cb1806bdf631b261532e1f54c54721d0525489f7
pranil chhetri <pranilchhetri2014@gmail.com>
2019-04-01 17:08:34 +0545

    latest

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,7 +5,7 @@ class SearchBar extends Component{
     constructor(props){
         super(props);
 
-        this.state ={ term: '' };
+        this.state ={ term: 'heey you' };
     }
 
     render(){
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDGfFJ1CybRVDuzt40j6abXaAy81wufubQ';
+
+YTSearch({key: API_KEY, term})
 
 //create a new component.producing some html
 const App= function(){
